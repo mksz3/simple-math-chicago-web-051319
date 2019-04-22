e167254dbccd2cb344cd430a1f6a1c6000f80191
Markus Sanders <markus.sanderst@gmail.com>
2019-04-22 18:42:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,10 +3,11 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  puts 
+  puts num1 - num2
 end
 
 def division(num1, num2)
+  puts num1 / num2
 end
 
 def multiplication(num1, num2)
